b86425c1a03d21d751606cc71cba6bd43a83a341
swathinchakravarthy <43194802+swathinchakravarthy@users.noreply.github.com>
2018-09-26 14:14:31 -0700

    Update my_answers.py

--- my_answers.py
+++ my_answers.py
@@ -15,17 +15,11 @@ class NeuralNetwork(object):
         self.weights_hidden_to_output = np.random.normal(0.0, self.hidden_nodes**-0.5, 
                                        (self.hidden_nodes, self.output_nodes))
         self.lr = learning_rate
-        
-        #### TODO: Set self.activation_function to your implemented sigmoid function ####
-        #
+       
         # Note: in Python, you can define a function with a lambda expression,
         # as shown below.
         self.activation_function = lambda x : 1/(1 + np.exp(-x))  # Replace 0 with your sigmoid calculation.
         
-        ### If the lambda code above is not something you're familiar with,
-        # You can uncomment out the following three lines and put your 
-        # implementation there instead.
-        #
         #def sigmoid(x):
         #    return 0  # Replace 0 with your sigmoid calculation here
         #self.activation_function = sigmoid
@@ -60,13 +54,13 @@ class NeuralNetwork(object):
             ---------
             X: features batch
         '''
-        #### Implement the forward pass here ####
+      
         ### Forward pass ###
         # TODO: Hidden layer - Replace these values with your calculations.
         hidden_inputs = np.dot(X, self.weights_input_to_hidden) # signals into hidden layer
         hidden_outputs = self.activation_function(hidden_inputs) # signals from hidden layer
 
-        # TODO: Output layer - Replace these values with your calculations.
+        #Output layer 
         final_inputs = np.dot(hidden_outputs, self.weights_hidden_to_output) # signals into final output layer
         final_outputs = final_inputs # signals from final output layer
         
@@ -82,16 +76,16 @@ class NeuralNetwork(object):
             delta_weights_i_h: change in weights from input to hidden layers
             delta_weights_h_o: change in weights from hidden to output layers
         '''
-        #### Implement the backward pass here ####
+      
         ### Backward pass ###
 
-        # TODO: Output error - Replace this value with your calculations.
+        #Output error 
         error = y - final_outputs # Output layer error is the difference between desired target and actual output.
         
         # TODO: Calculate the hidden layer's contribution to the error
         hidden_error = np.dot(self.weights_hidden_to_output, error) 
         
-        # TODO: Backpropagated error terms - Replace these values with your calculations.
+        #Backpropagated error terms 
         output_error_term = error
         
         hidden_error_term = hidden_error * hidden_outputs * (1 - hidden_outputs)
@@ -123,11 +117,11 @@ class NeuralNetwork(object):
         '''
         
         #### Implement the forward pass here ####
-        # TODO: Hidden layer - replace these values with the appropriate calculations.
+        #Hidden layer - replace these values with the appropriate calculations.
         hidden_inputs =  np.dot(features, self.weights_input_to_hidden)
         hidden_outputs = self.activation_function(hidden_inputs)
         
-        # TODO: Output layer - Replace these values with the appropriate calculations.
+        #Output layer
         final_inputs = np.dot(hidden_outputs, self.weights_hidden_to_output)
         final_outputs = final_inputs 
         
